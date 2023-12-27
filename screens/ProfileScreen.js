f1aad9dd2d5da4c69c0a683db17ab3d044e72427
Khobi <kwabena.adu505@outlook.com>
2023-12-27 01:31:56 +0000

    Work done on profile page and new images added

--- screens/ProfileScreen.js
+++ screens/ProfileScreen.js
@@ -1,10 +1,53 @@
-import { View, Text } from 'react-native'
+import { View, Text, Image, SafeAreaView, ImageBackground, TouchableOpacity} from 'react-native'
 import React from 'react'
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+
 
 export default function ProfileScreen() {
   return (
-    <View>
-      <Text>ProfileScreen</Text>
-    </View>
+    <SafeAreaView>
+    <ImageBackground
+    source={require("../assets/Images/green01unsplash.jpg")}
+    style={{
+      width: '100%', // applied to Image
+      height: '100%' 
+    }}>
+      
+      <View className='bg-white mt-40' style={{
+      width: '100%', 
+      height: '100%' 
+    }}>
+      {/* <View className='mb-5'> 
+          <Image className='mb-5'source={require("../assets/Images/starlife.png")}/>
+      </View> */}
+      <View className='items-center mb-5'>
+        <Text className='text-xl text-[#115E67] font-bold'>Kwabena AT</Text>
+        <Text className='text-lg'>055 689 9376</Text>
+        <Text className='text-base'>kadu-tekyi001@st.ug.edu.gh</Text>
+      </View>
+      <View>
+        <Text className='mx-5 text-xl mb-2 text-[#115E67] font-bold'>Dashboard</Text>
+        <View className='bg-[#115E67] mx-5 rounded-3xl flex-row' style={{height:'30%'}}> 
+        <View className='mx-8 my-auto'>
+          <MaterialCommunityIcons  name='notebook'size={50} color={'white'}/>
+        </View>
+        <View className='bg-white flex-col rounded-full my-auto' style={{height:'60%', width:'2%'}}></View>
+        <View className='flex-col mx-4 text-white my-auto'>
+          <Text className='text-white text-base'> Policy :</Text>
+          <Text className='text-white text-base'>Type    :</Text>
+        </View>
+        </View>
+      </View>
+      <View>
+        <Text className='mx-5 text-xl mb-2 text-[#115E67] font-bold'>Settings</Text>
+        <TouchableOpacity>
+
+        </TouchableOpacity>
+      </View>
+
+      </View>
+    {/* <Text>Some Content</Text> */}
+</ImageBackground>
+</SafeAreaView>
   )
 }
