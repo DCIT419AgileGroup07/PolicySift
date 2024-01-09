afd6b94db66cc447eba743ff7cbe9c950745fe5c
Khobi <kwabena.adu505@outlook.com>
2024-01-09 00:06:15 +0000

    Updated AboutUs Page

--- screens/AboutUs.js
+++ screens/AboutUs.js
@@ -19,27 +19,45 @@ export default function AboutUs() {
         <Text className='text-white text-2xl font-semibold items-center'>About Us</Text>
         </View>
     </View>
-    <View className='items-center bg-[#115E67] my-16 mx-'>
+    <View className='items-center bg-white my-16 mx-4 rounded-3xl'>
         <ScrollView>
         <View className='items-center'><Image source={require("../assets/Images/logo.png")}/></View>
+        <View className='items-center mb-2'><Text className='text-[#115E67] text-xl text-center font-bold'>Introducing Policy Sift: Your One-Stop Insurance App</Text></View>
              <View className='mx-4'>
-              <Text className='text-white text-lg'>
-            Travelling is always an exciting activity be it for leisure or business. 
-            The fun and excitement come with the responsibility to make your journey safe. 
-            Your desire to have a fun-filled travel without worries is what we aim to achieve. 
-            Star's Travel Insurance provides cover for travelers when they leave the shores of Ghana 
-            against a broad range of mishaps and host of unforeseen events that may occur in the course of their trip.
-            Benefits The policy provides cover for Medical, Emergency Service & Repatriation up to €30,000.
-            Additionally, we cover:{"\n"}
-            · Hospitalization{"\n"}
-            · Accidental death and permanent disablement{"\n"}
-            · Extended accommodation and travel expense{"\n"}
-            · Complimentary / Free Insurance Extension{"\n"}
-            · Loss of Personal Baggage{"\n"}
-            · Compensation for delay in the arrival of luggage{"\n"}
-            · Compensation (expense for replacement) for loss of essential documents{"\n"}
-            · Compensation for in-flight loss of checked-in luggage hijack Benefit{"\n"}
-            </Text>
+              <Text className='text-[#115E67] text-lg text-center'>
+              Policy Sift is a revolutionary mobile insurance application that is changing the way 
+              people manage their insurance policies. With Policy Sift, you can easily 
+              compare quotes from top insurance providers, track your existing policies, and 
+              file claims – all from the convenience of your smartphone.</Text>
+
+              <View className='my-2'><Text className='text-[#115E67] text-center text-xl font-bold'>
+                What Makes Policy Sift Different?</Text></View>
+
+                
+              <View><Text className='text-[#115E67] text-lg text-center font-bold'>Simplicity</Text>
+              <Text className='text-[#115E67] text-lg text-center'>Policy Sift is designed to be user-friendly 
+              and intuitive, even for those who are not familiar with insurance.</Text>
+              </View>
+
+              <View><Text className='text-[#115E67] text-lg text-center font-bold'>Choice</Text>
+              <Text className='text-[#115E67] text-lg text-center'> Compare quotes from multiple insurance providers 
+              to find the best coverage at the best price.
+              </Text>
+              </View>
+
+              <View><Text className='text-[#115E67] text-lg text-center font-bold'>Convenience</Text>
+              <Text className='text-[#115E67] text-lg text-center'> Manage all of your insurance 
+              policies in one place, anytime, anywhere..</Text>
+              </View>
+
+              <View><Text className='text-[#115E67] text-lg text-center font-bold'>Control</Text>
+              <Text className='text-[#115E67] text-lg text-center'> Take control of your insurance 
+              claims with our easy-to-use claims filing process</Text>
+              </View>
+
+              
+        
+            
             </View>
         </ScrollView>    
     </View>
