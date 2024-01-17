8ab5a7360b1f37f4910e4981446c047e07cf1535
Khobi <kwabena.adu505@outlook.com>
2024-01-17 13:12:17 +0000

    Padding and margin updated

--- screens/AllianzGeneralMain.js
+++ screens/AllianzGeneralMain.js
@@ -19,33 +19,33 @@ export default function AllianzGeneralMain() {
             <Image source={require("../assets/Images/Allianz.png")} />
         </View>
 
-        <View className="py-4 bg-[#115E67] mb-7 mx-20 rounded-xl items-center">
+        <View className="p-4 bg-[#115E67] mb-7 mx-20 rounded-xl items-center">
              <Text className='font-bold text-white'>ALLIANZ GENERAL INSURANCE</Text>
         </View>
 
-        <View className="flex-row">
+        <View className="flex-row mx-7">
            
 
-           <TouchableOpacity onPress={() => navigation.navigate("AllianzMotor")}  style={{width: 155, height: 130}} className="bg-teal-50 elevation mt-7  ml-8  rounded-l items-center">
+           <TouchableOpacity onPress={() => navigation.navigate("AllianzMotor")}  style={{width: 155, height: 130}} className="bg-teal-50 elevation mt-7 rounded-l items-center">
               <Image source={require("../assets/Images/motor.png")} className='mt-7' style={{width: 70, height: 50}} />
               <Text className='text-teal-800 mt-1 text-xs text-center font-medium'>Motor Insurance</Text>
               
             </TouchableOpacity>
 
-            <TouchableOpacity onPress={() => navigation.navigate("AllianzHomePolicy")} className="bg-teal-50 elevation mt-7  mx-7 rounded-l items-center" style={{width: 155, height: 130}}>
+            <TouchableOpacity onPress={() => navigation.navigate("AllianzHomePolicy")} className="bg-teal-50 elevation mt-7 mx-4 rounded-l items-center" style={{width: 155, height: 130}}>
             <Image source={require("../assets/Images/Familyinsurance.png")} className='mt-6' style={{width: 70, height: 50}} />            
              
-              <Text className='text-teal-800 mt-3 text-xs  text-center font-medium'>Home Insuarance  </Text>
+              <Text className='text-teal-800 mt-3 text-xs  text-center font-medium'>Home Insuarance</Text>
             </TouchableOpacity> 
         </View>
 
-        <View className="flex-row mb-5 ">
-           <TouchableOpacity onPress={() => navigation.navigate("AllianzTravel")} style={{width: 155, height: 130}} className="bg-teal-50 elevation mt-7  ml-8  rounded-l items-center">
+        <View className="flex-row mb-5 mx-7">
+           <TouchableOpacity onPress={() => navigation.navigate("AllianzTravel")} style={{width: 155, height: 130}} className="bg-teal-50 elevation mt-7 rounded-l items-center">
               <Image source={require("../assets/Images/plane.png")} className='mt-6' style={{width: 130, height: 50}} />
               <Text className='text-teal-800 mt-1 text-center text-xs font-medium'> Travel Insurance </Text>
             </TouchableOpacity>
 
-            <TouchableOpacity style={{width: 155, height: 130}} className="bg-teal-50 elevation mt-7  ml-8  rounded-l items-center">
+            <TouchableOpacity style={{width: 155, height: 130}} className="bg-teal-50 elevation mt-7 mx-4 rounded-l items-center">
               <Image source={require("../assets/Images/funeralservice_1.png")}  className='mt-5' style={{width: 70, height: 50}} />
               <Text className='text-teal-800 mt-1 text-center text-xs font-medium'>GLICO Funeral {"\n"} Policy </Text>
             </TouchableOpacity> 
