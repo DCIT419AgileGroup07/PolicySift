aeccc73b094cf74cf841b93b4718ab0097a58f05
Khobi <kwabena.adu505@outlook.com>
2024-01-17 13:30:16 +0000

    Padding and margin updated

--- screens/AllianzGeneralMain.js
+++ screens/AllianzGeneralMain.js
@@ -19,7 +19,7 @@ export default function AllianzGeneralMain() {
             <Image source={require("../assets/Images/Allianz.png")} />
         </View>
 
-        <View className="p-4 bg-[#115E67] mb-7 mx-20 rounded-xl items-center">
+        <View className="p-4 bg-[#115E67] mb-7 mx-8 rounded-xl items-center">
              <Text className='font-bold text-white'>ALLIANZ GENERAL INSURANCE</Text>
         </View>
 
@@ -41,13 +41,13 @@ export default function AllianzGeneralMain() {
 
         <View className="flex-row mb-5 mx-7">
            <TouchableOpacity onPress={() => navigation.navigate("AllianzTravel")} style={{width: 155, height: 130}} className="bg-teal-50 elevation mt-7 rounded-l items-center">
-              <Image source={require("../assets/Images/plane.png")} className='mt-6' style={{width: 130, height: 50}} />
+              <Image source={require("../assets/Images/plane.png")} className='mt-6' style={{width: 135, height: 50}} />
               <Text className='text-teal-800 mt-1 text-center text-xs font-medium'> Travel Insurance </Text>
             </TouchableOpacity>
 
             <TouchableOpacity style={{width: 155, height: 130}} className="bg-teal-50 elevation mt-7 mx-4 rounded-l items-center">
               <Image source={require("../assets/Images/funeralservice_1.png")}  className='mt-5' style={{width: 70, height: 50}} />
-              <Text className='text-teal-800 mt-1 text-center text-xs font-medium'>GLICO Funeral {"\n"} Policy </Text>
+              <Text className='text-teal-800 mt-1 text-center text-xs font-medium'>Allianz Funeral {"\n"} Policy </Text>
             </TouchableOpacity> 
         </View>
 
