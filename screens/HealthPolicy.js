4a8c2e8d23d336b3db62b63e2f18b78379dc9cb5
Maame Akua Affainie <maaffainie08@gmail.com>
2024-01-17 19:38:55 +0000

    StarLife Health Update

--- screens/HealthPolicy.js
+++ screens/HealthPolicy.js
@@ -16,7 +16,7 @@ export default function HealthPolicy() {
 
     
       <View className='mt-5'>
-        <TouchableOpacity>
+        <TouchableOpacity onPress={() => navigation.navigate("StarLifeHealthMain")}>
               <View className='mb-7' style={{height: 1, backgroundColor: 'black'}}></View>
               <View className='flex-row justify-center'>
                 <Image source={require("../assets/Images/starlife.png")}/>
