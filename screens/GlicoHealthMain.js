f7473cc2a7937c7a94063d02f2f234a2d730261e
khobi505 <kwabena.adutekyi505@outlook.com>
2024-01-18 15:13:36 +0000

    Glico health WIP

--- screens/GlicoHealthMain.js
+++ screens/GlicoHealthMain.js
@@ -31,7 +31,42 @@ export default function GlicoHealthMain() {
         <Text className="font-extrabold mt-5 text-teal-900 text-base">
         GLICO HEALTH INSURANCE
         </Text>
-      </View>
+        </View>
+
+        <View className='bg-[#115E67] mt-5 mx-5 rounded-xl'>
+        <View className="flex-row m-5">
+          <TouchableOpacity
+            // onPress={() => navigation.navigate("")} 
+            style={{ width: 155, height: 130 }}
+            className=" bg-[#43A9AF] elevation mt-7 rounded-l items-center"
+          >
+            <Image
+              source={require("../assets/Images/image3.png")}
+              className="mt-7"
+              style={{ width: 70, height: 50 }}
+            />
+            <Text className="text-white mt-1 text-xs text-center font-medium">
+            Glico Health Plan
+            </Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity
+            // onPress={() => navigation.navigate("")} 
+            style={{ width: 155, height: 130 }}
+            className=" bg-[#43A9AF] elevation mt-7  mx-7 rounded-l items-center"
+          >
+            <Image
+              source={require("../assets/Images/image6.png")}
+              className="mt-7"
+              style={{ width: 70, height: 50 }}
+            />
+            <Text className="text-white mt-1 text-xs text-center font-medium">
+            Glico Top-Up Scheme
+            </Text>
+          </TouchableOpacity>
+        </View>
+        </View>
+      
       </SafeAreaView>
   )
 }
